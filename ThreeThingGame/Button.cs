59e9ebdf72290a5f2b0e7292e17f754d2d603b4f
777347 <777347@FEN-052-A-40>
2023-11-25 13:25:29 +0000

    Began work on main menu.

--- ThreeThingGame/Button.cs
+++ ThreeThingGame/Button.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +17,7 @@ namespace ThreeThingGame
         private Rectangle _rect;
         private ClickEvent _onClick; 
         // Callback method
-        public delegate void ClickEvent();
+        public delegate void ClickEvent(ref uint state);
 
         // Constructors
         public Button()
--- ThreeThingGame/Menu.cs
+++ ThreeThingGame/Menu.cs
@@ -16,24 +16,55 @@ namespace ThreeThingGame
         private SpriteBatch _spriteBatch;
 
         // Internal Variables
+        List<Button> buttons = new List<Button>();
 
 
         // Constructor
-        public Menu(ref GraphicsDeviceManager graphics, ref SpriteBatch spriteBatch)
+        public Menu(ref GraphicsDeviceManager graphics, ref SpriteBatch spriteBatch, Texture2D texture)
         {
             _graphics = graphics;
             _spriteBatch = spriteBatch;
+            Button newGame = new Button(
+                "NEW GAME", 
+                false, 
+                texture, 
+                new Rectangle(325, 300, 250, 100), 
+                NewGameStart
+                );
+            buttons.Add(newGame);
+            Button exitGame = new Button(
+                "X",
+                false,
+                texture,
+                new Rectangle(935, 0, 25, 25),
+                ExitGame
+                );
+            buttons.Add(exitGame);
         }
 
         // Methods
-        public void RunLogic()
+        public void RunLogic(ref uint state)
         {
             // Run logic here
         }
         public void RunGraphics(SpriteBatch spriteBatch,
-            SpriteFont SWTxt_36)
+            SpriteFont SWTxt_36,
+            Texture2D titleTexture
+            )
         {
             spriteBatch.DrawString(SWTxt_36, "Test text", new Vector2(10,10), Color.White);
+
+            spriteBatch.Draw(titleTexture, new Rectangle(320, 160, 400, 300), Color.White);
+        }
+
+        private void NewGameStart(ref uint state)
+        {
+            state = 1;
+        }
+
+        private void ExitGame(ref uint state)
+        {
+            Exit();
         }
     }
 }
